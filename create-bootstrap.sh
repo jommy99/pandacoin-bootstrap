3b7e298211f2e25dac0d9df0cd6ef7fbc20863cc
amDOGE <kaname.madoka@outlook.com>
2018-02-04 22:06:28 +0100

    get rid of pushd

--- create-bootstrap.sh
+++ create-bootstrap.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 set -xe
 DATA="/etc/docker/compose/pandacoind/data"
-pushd $DATA
-ls -1 blk[0-9]*.dat | sort | xargs cat > bootstrap.dat
-popd
-rm bootstrap*
-mv $DATA/bootstrap.dat .
+ls -1 $DATA/blk[0-9]*.dat | sort | xargs cat > bootstrap.dat
+rm bootstrap.zip
 zip bootstrap.zip bootstrap.dat
 git commit -am "Autobootstrap: $(date '+%Y-%m-%d %H:%M:%S')"
 git push origin master
